c4481376ff092129c6687c1f6b256a09fc9bbaf3
BillCarioca <billcarioca@hotmail.com>
2023-02-13 14:53:05 -0300

    Estruras condicionais

---
+++ EstruturasCondicionais/src/edu/bill/condicionais/Condicionais.java
@@ -0,0 +1,25 @@
+package edu.bill.condicionais;
+
+import java.util.Scanner;
+
+public class Condicionais {
+
+	public static void main(String[] args)  {
+		
+		int nota;
+		String situacaoAluno;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a nota do aluno: ");
+		nota = sc.nextInt();
+		
+		if (nota>=0 && nota<=10) { 
+			situacaoAluno = nota>= 6 ? "Aprovado":"Reprovado";
+			System.out.println("Aluno está com nota: " + nota + " situação: " + situacaoAluno);
+		}else System.out.println("Nota invalida");
+		
+		sc.close();
+	}
+
+}
---
+++ EstruturasCondicionais/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Marcelino
+ *
+ */
+module EstruturasCondicionais {
+}
